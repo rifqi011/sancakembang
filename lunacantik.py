5ef8d48e63e8177cbdf15ede0cfcecb888b8540d
rifqi011 <rifqibanusafingi11@gmail.com>
2024-03-27 13:21:07 +0700

    update

--- lunacantik.py
+++ lunacantik.py
@@ -1,1 +1,11 @@
-print("Luna Cantik")
+while True:
+    tanya = input("Tanyakan Sesuatu: ")
+    
+    if tanya == "who is beautiful person in the world":
+        print("Luna")
+
+    elif tanya == "is that true":
+        print("yes")
+    
+    elif tanya == "selesai":
+        break
